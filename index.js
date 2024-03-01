24b1611e7a4287649a1913368af9695b0df8c3a5
mubeenshahid03 <91826576+mubeenshahid03@users.noreply.github.com>
2024-03-01 19:40:49 +0500

    in index.js file comment multer functionality

--- index.js
+++ index.js
@@ -26,34 +26,34 @@ app.use("/api/bookings",require("./Routes/bookings"))
 
 
 
-//code for multer setup and use
-app.use("/", express.static("uploads"));
-// Set up Multer storage
-const storage = multer.diskStorage({
-  destination: "./uploads/",
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
+// //code for multer setup and use
+// app.use("/", express.static("uploads"));
+// // Set up Multer storage
+// const storage = multer.diskStorage({
+//   destination: "./uploads/",
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+//     );
+//   },
+// });
 
-// Create Multer instance
-const upload = multer({ storage });
+// // Create Multer instance
+// const upload = multer({ storage });
 
-// Handle image upload
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    const imagePath = req.file.filename;
-    // Save imagePath to your database
-    console.log(imagePath + "ok");
-    // Respond with the imagePath
-    res.json({ imagePath });
-  } catch (error) {
-    console.error("Error uploading image:", error);
-  }
-});
+// // Handle image upload
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   try {
+//     const imagePath = req.file.filename;
+//     // Save imagePath to your database
+//     console.log(imagePath + "ok");
+//     // Respond with the imagePath
+//     res.json({ imagePath });
+//   } catch (error) {
+//     console.error("Error uploading image:", error);
+//   }
+// });
 
 connectToMongoose();
 app.get("/", (req, res) => {
